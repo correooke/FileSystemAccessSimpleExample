b5743686d27b85fa4b191c5458ea5b5bd2a74bd5
HSNBDES33\eocariz <eocariz@hasar.com>
2020-10-23 10:23:26 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+async function action(contents) {
+  async function getFileHandle() {
+    const opts = {
+      types: [
+        {
+          description: 'Text Files',
+          accept: {
+            'text/plain': ['.txt', '.text'],
+            'text/html': ['.html', '.htm']
+          }
+        }
+      ]
+    };
+    return await window.showOpenFilePicker(opts);
+  }
+  async function saveFile(fileHandle, text) {
+    if (!fileHandle) {
+      fileHandle = await window.showSaveFilePicker();
+    }
+    const writable = await fileHandle.createWritable();
+    await writable.write(text);
+    await writable.close();
+  }
+  debugger
+  const fHandle = await getFileHandle();
+  const file = await fHandle[0].getFile();
+  const text = await file.text();
+  saveFile(fHandle[0], contents + text);
+}
+
 function App() {
+  const [state, setState] = useState("")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={state} onChange={e => setState(e.currentTarget.value)} />
+      <button onClick={() => action(state)}>Click</button>
     </div>
   );
 }
